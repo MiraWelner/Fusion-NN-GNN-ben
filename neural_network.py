Model works okay but averages 2 inputs - I fixed the bug where I had the inputs and outputs in wrong order such that the model was trained on ecg and tested on rr - shorted input - converted to to zscore normalization

neural_network.py
@@ -4,12 +4,16 @@ import torch.nn as nn
 
 # Sub-network A
 class heartrate_net(nn.Module):
-    def __init__(self):
-        super(heartrate_net, self).__init__()
+    def __init__(self, input_dim=3000):
+        super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(3000, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16)
+            nn.Linear(input_dim, 2048),
+            nn.GELU(),
+            nn.Linear(2048, 1024),
+            nn.GELU(),
+            nn.Linear(1024, 512),
+            nn.GELU(),
+            nn.Linear(512, 256),
         )
 
     def forward(self, x):
@@ -17,46 +21,58 @@ class heartrate_net(nn.Module):
 
 # Sub-network B
 class ecg_net(nn.Module):
-    def __init__(self):
-        super(ecg_net, self).__init__()
+    def __init__(self, input_dim=3000):
+        super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(3000, 32),
-            nn.ReLU(),
-            nn.Linear(32, 32),
-            nn.ReLU(),
-            nn.Linear(32, 32),
-            nn.ReLU(),
-            nn.Linear(32, 16)
+            nn.Linear(input_dim, 2048),
+            nn.GELU(),
+            nn.Linear(2048, 1024),
+            nn.GELU(),
+            nn.Linear(1024, 512),
+            nn.GELU(),
+            nn.Linear(512, 256),
         )
 
     def forward(self, x):
         return self.net(x)
 
-# Fusion Network
 class combo_net(nn.Module):
     def __init__(self, output_len=3000):
-        super(combo_net, self).__init__()
-        self.shared = nn.Sequential(
-            nn.Linear(32, 16),
-            nn.ReLU()
+        super().__init__()
+        self.hr_proj = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.GELU()
         )
-        self.out1 = nn.Linear(16, output_len)
-        self.out2 = nn.Linear(16, output_len)
+        self.ecg_proj = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.GELU()
+        )
+
+        self.fusion = nn.Sequential(
+            nn.Linear(256 * 2, 512),
+            nn.GELU(),
+            nn.Linear(512, 1024),
+            nn.GELU(),
+            nn.Linear(1024, 2048),
+            nn.GELU(),
+            nn.Linear(2048, output_len)
+        )
 
     def forward(self, a_out, b_out):
-        combined = torch.cat((a_out, b_out), dim=1)
-        fused = self.shared(combined)
-        return self.out1(fused), self.out2(fused)
+        a = self.hr_proj(a_out)
+        b = self.ecg_proj(b_out)
+        fused = torch.cat([a, b], dim=1)  # [B, 32]
+        output = self.fusion(fused)       # [B, 3000]
+        return output, output  # return both outputs for now
 
-# Combine everything into a model wrapper
 class GNN(nn.Module):
-    def __init__(self):
-        super(GNN, self).__init__()
-        self.a = heartrate_net()
-        self.b = ecg_net()
-        self.fusion = combo_net()
+    def __init__(self, signal_length):
+        super().__init__()
+        self.a = heartrate_net(input_dim=signal_length)
+        self.b = ecg_net(input_dim=signal_length)
+        self.fusion = combo_net(output_len=signal_length)
 
     def forward(self, x1, x2):
-        out_a = self.a(x1)
-        out_b = self.b(x2)
-        return self.fusion(out_a, out_b)  # returns y1_pred, y2_pred
+        out_a = self.a(x1)  # [B, 16]
+        out_b = self.b(x2)  # [B, 16]
+        return self.fusion(out_a, out_b)  # [B, 3000], [B, 3000]

process_data.py
@@ -17,7 +17,12 @@ import itertools
 
 frac_sec = 10 #the x values are 10th of a second
 ecg_hz = 500
-snip_len = 6000
+snip_len = 2500
+
+def zscore_normalize(x):
+    mean = np.mean(x, keepdims=True)
+    std = np.std(x, keepdims=True)
+    return (x - mean) / (std + 1e-8)
 
 def process_rr(rr_distance_ms):
     """
@@ -25,7 +30,7 @@ def process_rr(rr_distance_ms):
     of
     """
     bpm = 60000/rr_distance_ms
-    scaled_heartrate = bpm/75 -1
+    scaled_heartrate = zscore_normalize(bpm)
     num_samples = len(scaled_heartrate)//snip_len
     scaled_heartrate_trimmed = scaled_heartrate[:num_samples*snip_len]
     heartrate_snips = scaled_heartrate_trimmed.reshape(num_samples, snip_len)
@@ -65,13 +70,13 @@ for i in range(2,5): #no ppg data for patient 1
     interpolated_ppg_for_patient = np.interp(np.linspace(0,1,len(ecg_file)), np.linspace(0,1,len(ppg_for_patient)), ppg_for_patient)
     all_ppg_data = np.append(all_ppg_data,interpolated_ppg_for_patient)
 
-all_ecg_data = 2 * (all_ecg_data - all_ecg_data.min()) / (all_ecg_data.max() - all_ecg_data.min() + 1e-8) - 1
+all_ecg_data = zscore_normalize(all_ecg_data)
 num_ecg_samples = len(all_ecg_data)//snip_len
 ecg_trimmed = all_ecg_data[:num_ecg_samples*snip_len]
 ecg_snips = ecg_trimmed.reshape(num_ecg_samples, snip_len)
 np.savetxt("processed_data/ecg.csv", ecg_snips, delimiter = ",")
 
-all_ppg_data = 2 * (all_ppg_data - all_ppg_data.min()) / (all_ppg_data.max() - all_ppg_data.min() + 1e-8) - 1
+all_ppg_data = zscore_normalize(all_ppg_data)
 num_ppg_samples = len(all_ppg_data)//snip_len
 ppg_trimmed = all_ppg_data[:num_ppg_samples*snip_len]
 ppg_snips = ppg_trimmed.reshape(num_ppg_samples, snip_len)

train_network.py
@@ -5,49 +5,46 @@ import matplotlib.pyplot as plt
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
+signal_length = 2500
+batch_size = 128*4
 
 
 ecg_data =np.loadtxt('processed_data/ecg.csv', delimiter=',',dtype=np.float32)
-ppg_data = np.loadtxt('processed_data/ppg.csv', delimiter=',',dtype=np.float32)
-qt_data =np.loadtxt('processed_data/qt.csv', delimiter=',',dtype=np.float32)
 rr_data = np.loadtxt('processed_data/rr.csv', delimiter=',',dtype=np.float32)
 
-ecg_data_train = torch.from_numpy(ecg_data[:,3000:])
-ecg_data_test = torch.from_numpy(ecg_data[:,:3000])
-ppg_data_train = torch.from_numpy(ppg_data[:,3000:])
-ppg_data_test = torch.from_numpy(ppg_data[:,:3000])
-qt_data_train = torch.from_numpy(qt_data[:,3000:])
-qt_data_test = torch.from_numpy(qt_data[:,:3000])
-rr_data_train = torch.from_numpy(rr_data[:,3000:])
-rr_data_test = torch.from_numpy(rr_data[:,:3000])
+ecg_data_train = torch.from_numpy(ecg_data[:,signal_length//2:])
+ecg_data_test = torch.from_numpy(ecg_data[:,:signal_length//2])
+rr_data_train = torch.from_numpy(rr_data[:,signal_length//2:])
+rr_data_test = torch.from_numpy(rr_data[:,:signal_length//2])
 
-ecg_dl = DataLoader(TensorDataset(ecg_data_train, ecg_data_test), shuffle=False, batch_size=16,drop_last=True)
-ppg_dl = DataLoader(TensorDataset(ppg_data_train,ppg_data_test), shuffle=False, batch_size=16,drop_last=True)
-rr_dl = DataLoader(TensorDataset(rr_data_train, rr_data_test), shuffle=False, batch_size=16,drop_last=True)
-qt_dl = DataLoader(TensorDataset(qt_data_train,qt_data_test),shuffle=False,  batch_size=16,drop_last=True)
+print(ecg_data_train.shape[0], batch_size)
+print(rr_data_train.shape[0], batch_size)
 
-model = GNN()
+ecg_dl = DataLoader(TensorDataset(ecg_data_train, ecg_data_test), shuffle=False, batch_size=batch_size,drop_last=True)
+rr_dl = DataLoader(TensorDataset(rr_data_train, rr_data_test), shuffle=False, batch_size=batch_size,drop_last=True)
+
+model = GNN(signal_length=signal_length//2)
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 device = "cuda"
 model = model.to(device)
-"""
+
 # Training loop
-for epoch in range(1000):
+epochs = 500
+for epoch in range(epochs):
     model.train()
     running_loss = 0.0
 
-    for (x1, x2), (y1, y2) in zip(ecg_dl,rr_dl):
-        x1, x2 = x1.to(device), x2.to(device)
-        y1, y2 = y1.to(device), y2.to(device)
+    for (ecg_input, ecg_output), (rr_input, rr_output) in zip(ecg_dl,rr_dl):
+        ecg_input, ecg_output, rr_input, rr_output = ecg_input.to(device), ecg_output.to(device), rr_input.to(device), rr_output.to(device)
 
         # Forward pass
-        y1_pred, y2_pred = model(x1, x2)
+        ecg_pred, rr_pred = model(ecg_input, rr_input)
 
         # Compute individual losses and combine
-        loss1 = criterion(y1_pred, y1)
-        loss2 = criterion(y2_pred, y2)
+        loss1 = criterion(ecg_pred, ecg_output)
+        loss2 = criterion(rr_pred, rr_output)
         loss = loss1 + loss2
 
         optimizer.zero_grad()
@@ -56,11 +53,11 @@ for epoch in range(1000):
 
         running_loss += loss.item()
 
-    print(f"Epoch {epoch+1}/{1000}, Loss: {running_loss / len(ecg_dl):.4f}")
+    print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss / len(ecg_dl):.4f}")
 
 torch.save(model.state_dict(), 'model.pth')
-"""
-model = GNN().cuda()
+
+model = GNN(signal_length=signal_length//2).cuda()
 model.load_state_dict(torch.load('model.pth', weights_only=True))
 
 ecg_inputs, ecg_outputs = next(iter(ecg_dl))